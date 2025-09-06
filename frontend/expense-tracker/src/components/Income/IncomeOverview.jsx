43f34fa1f9be50d62feae7063cdd08bf03dda3d4
Abhay <2022.csai.2213033@gmail.com>
2025-09-06 21:52:30 +0530

    Add IncomeList and DeleteAlert components; update Income page to integrate new features

--- frontend/expense-tracker/src/components/Income/IncomeOverview.jsx
+++ frontend/expense-tracker/src/components/Income/IncomeOverview.jsx
@@ -12,29 +12,29 @@ const IncomeOverview = ({ transactions, onAddIncome }) => {
     setChartData(result);
   }, [transactions]);
 
-
-  return (
-    <div className='card'>
-      <div className="flex items-center justify-between">
-        <div>
-          <h5 className='text-lg'>Income Overview</h5>
-          <p className='text-xs text-gray-400 mt-0.5'>
-            Track your earnings over time and analyze your income trends.
-          </p>
-        </div>
-
-        <button className='add-btn' onClick={onAddIncome}>
-          <LuPlus className='text-lg' />
-          Add Income
-        </button>
+return (
+  <div className='card'>
+    <div className="flex items-center justify-between">
+      <div>
+        <h5 className='text-lg'>Income Overview</h5>
+        <p className='text-xs text-gray-400 mt-0.5'>
+          Track your earnings over time and analyze your income trends.
+        </p>
       </div>
 
-      <div className="mt-10 w-full">
-        <CustomBarChart
-          data={chartData}
-        />
-      </div>
+      <button className='add-btn' onClick={onAddIncome}>
+        <LuPlus className='text-lg' />
+        Add Income
+      </button>
     </div>
+
+    <div className="mt-10 w-full">
+      <CustomBarChart
+        data={chartData}
+      />
+
+    </div>
+  </div>
   )
 }
 
